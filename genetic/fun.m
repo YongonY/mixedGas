5e4404241ed552ed26b9ea5849b4aeb76164a714
YongonY <yangxiao_seu@163.com>
2017-02-22 13:32:32 +0800

    删除
    删除

--- genetic/fun.m
+++
@@ -1,8 +0,0 @@
-function ret=fun(chrom,fun_C,fun_diff)
-%函数功能计算染色体对应的适应度值，主程序中默认适应度（返回值）越小越好
-L = 100;
-ret = 0;
-for i = 1:length(fun_C)
-    ret = ret + abs(fun_C(i) - (chrom'*fun_diff(:,i))/(chrom'*chrom*L))/fun_C(i);
-end
-end
